69492c789cebd26bcf98e6e0c1df3f7a42bb216a
Waldemar Quevedo <wally@synadia.com>
2019-08-23 10:29:48 -0700

    First commit
    Signed-off-by: Waldemar Quevedo <wally@synadia.com>

---
+++ examples/async-client.rs
@@ -0,0 +1,29 @@
+//! TCP client.
+//!
+//! Run the client:
+//!
+//! ```sh
+//! $ cargo run --example async-client
+//! ```
+
+use async_std::io;
+use async_std::net::TcpStream;
+use async_std::prelude::*;
+use async_std::task;
+
+fn main() -> io::Result<()> {
+    task::block_on(async {
+        let mut stream = TcpStream::connect("127.0.0.1:4222").await?;
+        println!("Connected to {}", &stream.peer_addr()?);
+
+        let msg = "hello world";
+        println!("<- {}", msg);
+        stream.write_all(msg.as_bytes()).await?;
+
+        let mut buf = vec![0u8; 1024];
+        let n = stream.read(&mut buf).await?;
+        println!("-> {}\n", String::from_utf8_lossy(&buf[..n]));
+
+        Ok(())
+    })
+}
